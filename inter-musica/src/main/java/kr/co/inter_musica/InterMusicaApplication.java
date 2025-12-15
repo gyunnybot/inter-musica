a7b37af0a3a208b8151d0837c88eac2c81a469b1
gyunnybot <idjkda4034@gmail.com>
2025-12-15 13:50:14 +0900

    feat: start maven project

---
+++ inter-musica/src/main/java/kr/co/inter_musica/InterMusicaApplication.java
@@ -0,0 +1,13 @@
+package kr.co.inter_musica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterMusicaApplication.class, args);
+	}
+
+}
---
+++ inter-musica/src/test/java/kr/co/inter_musica/InterMusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.inter_musica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterMusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
