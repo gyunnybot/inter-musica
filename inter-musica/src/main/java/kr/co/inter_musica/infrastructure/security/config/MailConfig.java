39716bcb830e03cc98dafcfb9b2f77727001ef10
gyunnybot <gyunnybot4029@gmail.com>
2025-12-28 19:08:38 +0900

    feat: mail 관련 Bean 추가

---
+++ inter-musica/src/main/java/kr/co/inter_musica/infrastructure/security/config/MailConfig.java
@@ -0,0 +1,30 @@
+package kr.co.inter_musica.infrastructure.security.config;
+
+import java.util.Properties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(JavaMailSender.class)
+    public JavaMailSender javaMailSender(MailProperties properties) {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost(properties.getHost());
+        sender.setPort(properties.getPort());
+        sender.setUsername(properties.getUsername());
+        sender.setPassword(properties.getPassword());
+        sender.setProtocol(properties.getProtocol());
+        sender.setDefaultEncoding(String.valueOf(properties.getDefaultEncoding()));
+
+        Properties javaMailProperties = sender.getJavaMailProperties();
+        javaMailProperties.putAll(properties.getProperties());
+
+        return sender;
+    }
+}
